1113bc9a0248b239d641bfb6cbdb897e914a9979
krrish-singhal <krrishsinghal62@gmail.com>
2025-12-07 02:02:05 +0530

    fix css configurations

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        shop_light_pink: "#fcf0e4",
+        shop_dark_green: "#161d53",
+        shop_btn_dark_green: "#161d53",
+        shop_light_bg: "#f6f6f6",
+        shop_light_green: "#3ab8a3",
+        shop_orange: "#fb6c08",
+        darkColor: "#151515",
+        lightColor: "#52525b",
+        lightOrange: "#fca99b",
+        deal_bg: "#f1f3f8",
+      },
+      keyframes: {
+        fadeInUp: {
+          from: { opacity: "0", transform: "translateY(30px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeInDown: {
+          from: { opacity: "0", transform: "translateY(-30px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeInLeft: {
+          from: { opacity: "0", transform: "translateX(-30px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+        fadeInRight: {
+          from: { opacity: "0", transform: "translateX(30px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+        "slide-text": {
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "10%": { opacity: "1" },
+          "90%": { opacity: "1" },
+          "100%": { transform: "translateX(100%)", opacity: "0" },
+        },
+      },
+      animation: {
+        "fade-in-up": "fadeInUp 0.6s ease-out forwards",
+        "fade-in-down": "fadeInDown 0.6s ease-out forwards",
+        "fade-in-left": "fadeInLeft 0.6s ease-out forwards",
+        "fade-in-right": "fadeInRight 0.6s ease-out forwards",
+        "slide-text": "slide-text 3s linear forwards",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
+
